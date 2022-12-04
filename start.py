ad682c8f9b2ed377c48b5b03f67da61a9e7f744a
Sokirlov <soxwhite@gmail.com>
2022-12-04 08:49:44 +0200

    Create startup script

--- start.py
+++ start.py
@@ -1,4 +1,4 @@
-import os, sys
+import os, sys, time
 import subprocess
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ClerkFoodhub.settings")
 sys.path.insert(0, os.getcwd())
@@ -12,24 +12,33 @@ USER = os.getenv('ADM_USER')
 PASS = os.getenv('ADM_PASS')
 
 
+
 def start():
     try:
         # from users.models import CustomUser
         if len(User.objects.all()) >= 1:
             call_command("runserver", "0.0.0.0:8000")
         else:
+            print('----- ITS else work\n\n\n')
             call_command("migrate", interactive=False)
+            User.objects.create_superuser(USER, 'admin@myproject.com', PASS)
+            call_command("imperial-food")
+            print('food is parsed well')
+            call_command("runserver", "0.0.0.0:8000")
             # from django.contrib.auth import get_user_model
             # User = get_user_model()
-            User.objects.create_superuser(USER, 'admin@myproject.com', PASS)
             # subprocess.run(f"python manage.py createsuperuser && {USER} &&  && {PASS} && {PASS} && y")
-            call_command("runserver", "0.0.0.0:8000")
             # subprocess.run("python manage.py runserver 0.0.0.0:8000")
     except:
-
+        print('!!!!!!! EXCEPTION work\n\n\n')
         call_command("migrate", interactive=False)
         User.objects.create_superuser(USER, 'admin@myproject.com', PASS)
         call_command("runserver", "0.0.0.0:8000")
 
 if __name__=='__main__':
-    start()
+    while True:
+        try:
+            start()
+            break
+        except:
+            time.sleep(10)
