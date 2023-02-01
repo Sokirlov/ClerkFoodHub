67d53834617604bf066f7bf7d2776309417a18bb
Sokirlov <64358905+Sokirlov@users.noreply.github.com>
2023-02-01 18:43:57 +0200

    need update start script

--- start.py
+++ start.py
@@ -1,46 +1,93 @@
 import os, sys, time
-import subprocess
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ClerkFoodhub.settings")
 sys.path.insert(0, os.getcwd())
 import django
-from django.core.management import call_command
 django.setup()
+from django.core.management import call_command
 from django.contrib.auth import get_user_model
-User = get_user_model()
+print('2134')
 
-USER = os.getenv('ADM_USER')
-PASS = os.getenv('ADM_PASS')
+def get_user():
+    while True:
+        try:
+            uuser = get_user_model()
+            return uuser
+        except:
+            time.sleep(10)
+User = get_user()
+
+def create_main_user():
+    try:
+        USER = os.getenv('ADM_USER')
+    except NameError:
+        print('Environment variable "USER" not found\nSet as default variable "root".')
+        USER = 'root'
+
+    try:
+        PASS = os.getenv('ADM_PASS')
+    except NameError:
+        print('Environment variable "PASS" not found\nSet as default variable "root".')
+        PASS = 'root'
+    print('__________It`s new run______')
+    call_command("migrate", interactive=False)
+    User.objects.create_superuser(USER, 'admin@myproject.com', PASS)
+
+def update_caterings():
+    call_command("imperial-food")
 
 
+def run_django():
+    print('All data was updated.\nNow RUN.')
+    call_command("runserver", "0.0.0.0:8000")
+
+
+def load_demo():
+    call_command("migrate", interactive=False)
+    print('Try load data to DB site_settings')
+    call_command('loaddata', 'site_settings')
+    print('Try load data to DB users')
+    call_command('loaddata', 'users')
+    update_caterings()
+    print('Try load data to DB cart')
+    call_command('loaddata', 'cart')
 
 def start():
     try:
         from users.models import CustomUser
-        print(f' {len(User.objects.all())} CustomUser = {len(CustomUser.objects.all())}')
-        # from users.models import CustomUser
         if len(User.objects.all()) >= 1:
-            call_command("runserver", "0.0.0.0:8000")
+            print(f'User is more 0 now it`s {len(User.objects.all())}')
+            update_caterings()
+            run_django()
         else:
-            print('----- ITS else work\n\n\n')
-            call_command("migrate", interactive=False)
-            User.objects.create_superuser(USER, 'admin@myproject.com', PASS)
-            call_command("imperial-food")
-            print('food is parsed well')
-            call_command("runserver", "0.0.0.0:8000")
-            # from django.contrib.auth import get_user_model
-            # User = get_user_model()
-            # subprocess.run(f"python manage.py createsuperuser && {USER} &&  && {PASS} && {PASS} && y")
-            # subprocess.run("python manage.py runserver 0.0.0.0:8000")
-    except:
-        print('!!!!!!! EXCEPTION work\n\n\n')
-        call_command("migrate", interactive=False)
-        User.objects.create_superuser(USER, 'admin@myproject.com', PASS)
+            if os.getenv('LOAD_DEMO_DATA'):
+                print('Try load data to DB')
+                load_demo()
+                print('Data  will load at DB')
+            else:
+                create_main_user()
+                update_caterings()
+    except Exception as ex:
+        print(f'Exception is {ex}')
+        if os.getenv('LOAD_DEMO_DATA'):
+            load_demo()
+            print('Data  will load at DB')
+        else:
+            create_main_user()
+            update_caterings()
+    finally:
         call_command("runserver", "0.0.0.0:8000")
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
+    counter: int = 0
     while True:
         try:
             start()
             break
-        except:
-            time.sleep(10)
+        except Exception as exc:
+            if counter == 10:
+                exit('More 10 try`s')
+            else:
+                counter += 1
+                print(f'Exceptions - {exc}')
+                time.sleep(10)
