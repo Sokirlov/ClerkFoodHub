5c97e14283caf64e8ebb179ffeb00ff95f757476
Sokirlov <64358905+Sokirlov@users.noreply.github.com>
2023-02-28 12:10:49 +0200

    heroku 6+

--- ClerkFoodhub/settings.py
+++ ClerkFoodhub/settings.py
@@ -1,19 +1,11 @@
 import os
 import sys
-from django.test.runner import DiscoverRunner
-import dj_database_url
-
-IS_HEROKU = "DYNO" in os.environ
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 PROJECT_ROOT = os.path.dirname(__file__)
 sys.path.insert(0, os.path.join(PROJECT_ROOT, 'apps'))
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
-
-if IS_HEROKU:
-    ALLOWED_HOSTS = ["*"]
-else:
-    ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ["*"]
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -59,7 +51,7 @@ WSGI_APPLICATION = 'wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-MAX_CONN_AGE = 600
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -69,41 +61,15 @@ DATABASES = {
 
 if "DATABASE_URL" in os.environ:
     # Configure Django for DATABASE_URL environment variable.
-    DATABASES["default"] = dj_database_url.config(
-        conn_max_age=MAX_CONN_AGE, ssl_require=True)
-
-    # Enable test database if found in CI environment.
-    if "CI" in os.environ:
-        DATABASES["default"]["TEST"] = DATABASES["default"]
-
-# DATABASES = {
-#     'default': dj_database_url.parse(
-#         os.getenv('JAWSDB_MARIA_URL'),
-#         conn_max_age=500
-#     )
-# }
-# DATABASES = {
-#
-#     'default': {
-#         'CONN_MAX_AGE': 500,
-#         'ENGINE': 'django.db.backends.mysql',
-#         'HOST': os.getenv('DB_URL'),
-#         'NAME': os.getenv('DB_NAME'),
-#         'USER': os.getenv('DB_USER'),
-#         'PASSWORD': os.getenv('DB_PASSWORD'),
-#         'PORT': 3306,
-#     }
-# }
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': os.getenv('POSTGRES_DB'),
-#         'USER': os.getenv('POSTGRES_USER'),
-#         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-#         'HOST': os.getenv('DATABASE_URL'),
-#         'PORT': 5432,
-#     }
-# }
+    DATABASES["default"] = {
+        'CONN_MAX_AGE': 500,
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': os.getenv('DB_URL'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'PORT': 3306,
+    }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
